125044220c132e79107903f1017592ac57cd7e8d
Deepak Giri <idg+git@live.in>
2020-12-24 09:44:52 -0400

    first two days' solutions; update gitignore

---
+++ README.md
@@ -0,0 +1,3 @@
+solutions/programmes for https://adventofcode.com/2020
+- unoptimized quick solutions
+- started pretty late - on 23rd December 2020
---
+++ src/com/dpkg/Main.java
@@ -0,0 +1,10 @@
+package com.dpkg;
+
+import com.dpkg.adventofcode.y2020.Day2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Day2.calculatePart2();
+    }
+}
